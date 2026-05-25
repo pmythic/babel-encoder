e63f02817b9d02c9d893dba4c9630ca351351d0c
the-polymythic <141257388+tayyebisrar@users.noreply.github.com>
2026-05-25 22:55:35 +0100

    Refactor babel.py

--- babel.py
+++ babel.py
@@ -5,14 +5,15 @@ if __name__ == "__main__":
 
     parser.add_argument(
         '-f', '--file', '--filename',
-        dest='filename',
-        help='File to encode/decode'
+        action="store_true",
+        dest='file_provided',
+        help='Treat input as filename'
     )
 
     parser.add_argument(
         'input',
-        nargs='?',
-        help='Input string if no file is supplied'
+        nargs='+',
+        help='File name, or input (depending on if -f flag set)'
     )
 
     decode_group = parser.add_argument_group('decoding')
@@ -20,16 +21,20 @@ if __name__ == "__main__":
     decode_group.add_argument(
         '-d', '--decode',
         action='store_true',
-        help='Decode instead of encode'
+        help='Decode flag instead of encode'
     )
 
     decode_group.add_argument(
         '--offset',
         type=int,
         default=0,
+        required=False,
         help='Reveal offset when decoding'
     )
 
     args = parser.parse_args()
+
+    if args.offset and not args.decode:
+        parser.error("--offset requires --decode")
 
     print(args)

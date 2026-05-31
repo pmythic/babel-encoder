1be64431af58f21a7e949140e0f38f31a6ca9d2f
the-polymythic <141257388+tayyebisrar@users.noreply.github.com>
2026-05-31 14:30:46 +0100

    Implement basic chudnovsky

---
+++ chudnovsky.py
@@ -0,0 +1,40 @@
+import math
+
+def tostr(bigint):
+    if bigint == 0:
+        return "0"
+
+    digits = []
+    while bigint > 0:
+        digits.append(chr(ord('0') + bigint % 10))
+        bigint //= 10
+
+    digits.reverse()
+    return "".join(digits)
+
+def pi_chudnovsky(digits=100):
+    """
+    Calculate pi using Chudnovsky's series
+
+    This calculates it in fixed point, using the value for one passed in
+    """
+    one= 10**digits
+
+    k = 1
+    a_k = one
+    a_sum = one
+    b_sum = 0
+    C = 640320
+    C3_OVER_24 = C**3 // 24
+    while True:
+        a_k *= -(6*k-5)*(2*k-1)*(6*k-1)
+        a_k //= k*k*k*C3_OVER_24
+        a_sum += a_k
+        b_sum += k * a_k
+        k += 1
+        if a_k == 0:
+            break
+    total = 13591409*a_sum + 545140134*b_sum
+    pi = (426880*math.isqrt(10005 * one * one)*one) // total
+    p = tostr(pi)
+    return f"{p[0]}.{p[1:]}"
